8962d706bb8f1a396fa34b6d4176354b2212d2e9
EliweltonMoreira <eliwelton.moreira@outlook.com>
2022-02-26 20:50:19 -0300

    add first.go

---
+++ fundamentals/first/first.go
@@ -0,0 +1,22 @@
+// Executable programs start from the package main
+package main
+
+/*
+The Go codes are organized in packages
+and to use them it is necessary to declare one or more imports
+*/
+import "fmt"
+
+//The main gate of a Go program is the main function
+func main() {
+	fmt.Print("First ")
+	fmt.Print("Program!")
+
+	/*
+		About comments...
+
+		1) Prioritize readable code and make comments that add value!
+		2) Avoid obvious comments
+		3) During the course abuse the comments
+	*/
+}
